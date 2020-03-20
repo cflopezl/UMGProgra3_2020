09d09ffc5a0326a7297f43d7d2f513b30fe443b9
C-Lo 12 <39096685+cflopezl@users.noreply.github.com>
2020-03-20 15:47:47 -0600

    Arbol Binario de Búsqueda Seccion B
    Solamente hasta la implementación del método Insertar

---
+++ JavaAppArbolBinario/src/javaapparbolbinario/Comparador.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapparbolbinario;
+
+/**
+ *
+ * @author chris
+ */
+public interface Comparador {
+     boolean igualQue(Object q); 
+     boolean menorQue(Object q);  
+     boolean mayorQue(Object q);
+}
---
+++ JavaAppArbolBinario/src/javaapparbolbinario/Estudiante.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapparbolbinario;
+
+/**
+ *
+ * @author chris
+ */
+public class Estudiante implements Comparador{
+    int numMat;
+    String nombre;
+
+    public Estudiante(int numMat, String nombre) {
+        this.numMat = numMat;
+        this.nombre = nombre;
+    }
+
+    public int getNumMat() {
+        return numMat;
+    }
+
+    public void setNumMat(int numMat) {
+        this.numMat = numMat;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }    
+
+    @Override
+    public boolean igualQue(Object q){
+        Estudiante e = (Estudiante)q;
+        return this.numMat==e.numMat;
+    }
+    
+        @Override
+    public boolean menorQue(Object q){
+        Estudiante e = (Estudiante)q;
+        return this.numMat<e.numMat;
+    }
+    
+        @Override
+    public boolean mayorQue(Object q){
+        Estudiante e = (Estudiante)q;
+        return this.numMat>e.numMat;
+    }
+
+    
+}
